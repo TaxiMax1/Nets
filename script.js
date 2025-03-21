baf8c800fe68ef0a0250aa76e8467b8a97f563f6
TaxiMax1 <taximaxi1808@gmail.com>
2025-03-21 11:01:07 +0100

    updaye

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleIcon = document.querySelector('.svg-icon');
     const aside = document.querySelector('.aside-container');
+    const asidenav = document.querySelector('.asidenav-container');
     const gamblebox = document.querySelector('#casinohome');
     const gamblebox2 = document.querySelector('#basketball');
     const netslogo = document.getElementById("netslogo");
@@ -12,8 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (aside.classList.contains('expanded')) {
             netslogo.style.marginLeft = "300px";
+            asidenav.style.flexDirection = "row";
         } else {
             netslogo.style.marginLeft = "0";
+            asidenav.style.flexDirection = "column";
         }
     });
 });
